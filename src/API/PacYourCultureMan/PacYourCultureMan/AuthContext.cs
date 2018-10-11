7d6c108f70aee6685553cd9975d39e1afc800f99
Carl Vancluysen <Carl.Vancluysen@gmail.com>
2018-10-11 15:06:58 +0200

    Rest API init

---
+++ src/API/PacYourCultureMan/PacYourCultureMan/AuthContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PacYourCultureMan
+{
+        public class AuthContext : IdentityDbContext<Microsoft.AspNet.Identity.EntityFramework.IdentityUser>
+        {
+            public AuthContext()
+                : base("AuthContext")
+            {
+
+            }
+        }
+}
---
+++ src/API/PacYourCultureMan/PacYourCultureMan/TokenStartup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+[assembly: OwinStartup(typeof(PacYourCultureMan.TokenStartup))]
+namespace PacYourCultureMan
+{
+        public class TokenStartup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                HttpConfiguration config = new HttpConfiguration();
+                WebApiConfig.Register(config);
+                app.UseWebApi(config);
+            }
+
+        }
+}
