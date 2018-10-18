ee1274814f2f2552680aaddb71db5b7408c8f9a3
Carl Vancluysen <Carl.Vancluysen@gmail.com>
2018-10-18 17:08:48 +0200

    POST methods Registration/Authentication

--- src/Android/app/src/main/java/com/ap/pacyourcultureman/Login.java
+++ src/Android/app/src/main/java/com/ap/pacyourcultureman/Login.java
@@ -11,10 +11,24 @@ import android.widget.TextView;
 import com.google.android.gms.maps.SupportMapFragment;
 
 import org.apache.commons.validator.routines.EmailValidator;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Login extends Activity {
-    private String email;
-    private String password;
+    private String email, password, targetURL;
     Button btn_login, btn_register, btn_dev;
     EditText edit_email, edit_password;
     TextView errorChecker;
@@ -22,6 +36,7 @@ public class Login extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_form);
+        targetURL = "";
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
         btn_dev = findViewById(R.id.btn_dev);
@@ -35,7 +50,14 @@ public class Login extends Activity {
                 email = edit_email.getText().toString();
                 Boolean correctEmail = EmailValidator.getInstance().isValid(email);
                 if(correctEmail) {
-
+                    JSONObject loginParams = new JSONObject();
+                    try {
+                        loginParams.put("password", password);
+                        loginParams.put("email", email);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    executePost(targetURL,loginParams.toString());
                 }
                 else {
                     errorChecker.setVisibility(View.VISIBLE);
@@ -57,6 +79,68 @@ public class Login extends Activity {
                 startActivity(intent);
             }
         });
+    }
+    public String executePost(String targetURL,String urlParameters) {
+        int timeout=5000;
+        URL url;
+        HttpURLConnection connection = null;
+        try {
+            // Create connection
+
+            url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type",
+                    "application/json");
+
+            connection.setRequestProperty("Content-Length",
+                    "" + Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(timeout);
+            connection.setReadTimeout(timeout);
+
+            // Send request
+            DataOutputStream wr = new DataOutputStream(
+                    connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            // Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+
+        } catch (SocketTimeoutException ex) {
+            ex.printStackTrace();
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return null;
     }
 
 
--- src/Android/app/src/main/java/com/ap/pacyourcultureman/Register.java
+++ src/Android/app/src/main/java/com/ap/pacyourcultureman/Register.java
@@ -8,17 +8,33 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import org.apache.commons.validator.routines.EmailValidator;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Register extends Activity {
     TextView txt_Errorchecker;
     Button btn_Register;
     EditText edit_user, edit_email, edit_firstName, edit_lastName, edit_password, edit_confirmPassword;
-    String username, email, firstName, lastName, password, confirmpassword, errorcheck;
+    String username, email, firstName, lastName, password, confirmpassword, errorcheck, targetURL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_form);
+        targetURL = "";
         txt_Errorchecker = findViewById(R.id.reg_txt_errorCecker);
         btn_Register = findViewById(R.id.reg_btn_register);
         edit_user = findViewById(R.id.reg_edit_username);
@@ -38,18 +54,104 @@ public class Register extends Activity {
                 confirmpassword = edit_confirmPassword.getText().toString();
                 txt_Errorchecker.setVisibility(View.GONE);
                 if(password != confirmpassword) {
-                    txt_Errorchecker.setText("Passwords need to match");
-                    txt_Errorchecker.setVisibility(View.VISIBLE);
+                    errorSetter("Passwords must match");
                 }
                 Boolean correctEmail = EmailValidator.getInstance().isValid(email);
                 if(!correctEmail) {
-                    txt_Errorchecker.setText("Please enter a valid email address");
-                    txt_Errorchecker.setVisibility(View.VISIBLE);
+                    errorSetter("Please enter a valid email address");
+                }
+                if(!passwordChecker(password)) {
+                    errorSetter("Password must include atleast 1 lowercase char, 1 uppercase char and 1 digit");
                 }
                 else {
-
+                    JSONObject loginParams = new JSONObject();
+                    try {
+                        loginParams.put("username", username);
+                        loginParams.put("password", password);
+                        loginParams.put("firstname", firstName);
+                        loginParams.put("lastname", lastName);
+                        loginParams.put("email", email);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    executePost(targetURL,loginParams.toString());
                 }
             }
         });
+    }
+    private boolean passwordChecker(String password) {
+        String regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}";
+        if(password.matches(regexp)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    private void errorSetter(String errormsg) {
+        txt_Errorchecker.setVisibility(View.VISIBLE);
+        txt_Errorchecker.setText(errormsg);
+    }
+    public String executePost(String targetURL,String urlParameters) {
+        int timeout=5000;
+        URL url;
+        HttpURLConnection connection = null;
+        try {
+            // Create connection
+
+            url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type",
+                    "application/json");
+
+            connection.setRequestProperty("Content-Length",
+                    "" + Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(timeout);
+            connection.setReadTimeout(timeout);
+
+            // Send request
+            DataOutputStream wr = new DataOutputStream(
+                    connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            // Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+
+        } catch (SocketTimeoutException ex) {
+            ex.printStackTrace();
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return null;
     }
 }
