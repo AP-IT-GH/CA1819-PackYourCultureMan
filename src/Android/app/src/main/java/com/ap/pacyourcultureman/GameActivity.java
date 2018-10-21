6e3b31324a31da04ae2cc8bf70d7e3225ac525b5
Jonas Luyks <jonas_luyks@hotmail.com>
2018-10-21 22:17:03 +0200

    added automatic location refresh

--- src/Android/app/src/main/java/com/ap/pacyourcultureman/GameActivity.java
+++ src/Android/app/src/main/java/com/ap/pacyourcultureman/GameActivity.java
@@ -2,27 +2,52 @@ package com.ap.pacyourcultureman;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GameActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.security.cert.LDAPCertStoreParameters;
+import java.util.List;
+
+public class GameActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
+
     private static final int MY_PERMISSIONS_REQUEST_ACCES_FINE_LOCATION = 1;
+
+    Location mLastLocation;
+    Location mCurrentLocation;
+    LocationRequest mLocationRequest;
+    Marker mCurrLocationMarker;
+    FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -39,16 +64,30 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        // Stops Location Updates if activity is paused
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng antwerp = new LatLng(51, 4);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(antwerp));
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); // 5 second interval
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
         } else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
@@ -66,8 +105,7 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_ACCES_FINE_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
@@ -80,6 +118,7 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
+                    // TODO: Disable Maps functionality
                 }
                 return;
             }
@@ -88,4 +127,32 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
             // permissions this app might request.
         }
     }
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            List<Location> locationList = locationResult.getLocations();
+            if (locationList.size() > 0) {
+                //The last location in the list is the newest
+                Location location = locationList.get(locationList.size() - 1);
+                Log.i("MapsActivity", "Location: " + location.getLatitude() + " " + location.getLongitude());
+                mLastLocation = location;
+                if (mCurrLocationMarker != null) {
+                    mCurrLocationMarker.remove();
+                }
+
+                //Place current location marker
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title("Current Position");
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+                mCurrLocationMarker = mMap.addMarker(markerOptions);
+
+                //move map camera
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11));
+            }
+        }
+    };
+
 }
