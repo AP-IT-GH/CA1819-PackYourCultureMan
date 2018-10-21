16e93f30987bc18e4f570b39462a725eb740875b
Unknown <cedricvanbroek@gmail.com>
2018-10-21 17:47:48 +0200

    routing works

--- src/Angular/packYourCultureMan/src/app/app-routing.module.ts
+++ src/Angular/packYourCultureMan/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-    {path: 'contact', component: ContactComponent}
+    {path: '', redirectTo: '/home',pathMatch:'full'},
+    {path: 'contact', component: ContactComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '**', component: NotFoundComponent},
+    
 ];
 @NgModule({
     imports:[RouterModule.forRoot(routes)],
     exports:[RouterModule]
 })
 export class AppRoutingModule{}
-export const routingComponents = [ContactComponent]
+export const routingComponents = [ContactComponent,HomeComponent,NotFoundComponent]
