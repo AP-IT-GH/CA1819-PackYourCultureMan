d0d6a89808ea8a42673924350840196733cfc50b
Unknown <cedricvanbroek@gmail.com>
2018-10-21 16:55:28 +0200

    routing

--- src/Angular/packYourCultureMan/src/app/app.module.ts
+++ src/Angular/packYourCultureMan/src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { UiPageModule } from './ui-page/ui-page.module';
+import{AppRoutingModule,routingComponents} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    UiPageModule
+    UiPageModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
