857d60b82b8054d9e422adcfa1e31f02657c3a1e
Carl Vancluysen <Carl.Vancluysen@gmail.com>
2018-11-02 20:10:14 +0100

    Hardcoded assigments + Random assigner

---
+++ src/Android/app/src/main/java/com/ap/pacyourcultureman/Assignment.java
@@ -0,0 +1,13 @@
+package com.ap.pacyourcultureman;
+
+public class Assignment {
+    String name,website, lat, lon, shortDescr, longDescr;
+    public Assignment(String name,String website, String lat, String lon, String shortDescr, String longDescr) {
+        this.name = name;
+        this.website = website;
+        this.lat = lat;
+        this.lon = lon;
+        this.shortDescr = shortDescr;
+        this.longDescr = longDescr;
+    }
+}
