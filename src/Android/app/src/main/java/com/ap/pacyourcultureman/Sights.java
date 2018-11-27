7d3bec37558f901eefed36f1b80d4c5982b15d11
Unknown <cedricvanbroek@gmail.com>
2018-11-27 13:22:26 +0100

    recyclevieuw

--- src/Android/app/src/main/java/com/ap/pacyourcultureman/Sights.java
+++ src/Android/app/src/main/java/com/ap/pacyourcultureman/Sights.java
@@ -2,12 +2,76 @@ package com.ap.pacyourcultureman;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.ap.pacyourcultureman.Helpers.SightsAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class Sights extends Activity {
+    private RecyclerView mRecyclerview;
+    private SightsAdapter mSighsAdapter;
+    private ArrayList<Assignment> mSightList;
+    private RequestQueue mRequestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sights_menu);
+        mRecyclerview = findViewById(R.id.recycler_view);
+        mRecyclerview.setHasFixedSize(true);
+        mRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+        mSightList = new ArrayList<>();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        parseJSON();
+    }
+
+    private void parseJSON() {
+        final String url = "https://api.myjson.com/bins/iilka";
+        JsonArrayRequest request = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                    try {
+                        for (int i = 0; i < response.length(); i++) {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        String name = jsonObject.getString("name");
+                        String website = jsonObject.getString("website");
+                        String shortDesc = jsonObject.getString("shortDescription");
+                        String longDesc = jsonObject.getString("longDescription");
+                        String imgUrl = jsonObject.getString("sightImage");
+                        String lat = jsonObject.getString("latitude");
+                        String lng = jsonObject.getString("longitude");
+                        mSightList.add(new Assignment(name, website, Double.valueOf(lng), Double.valueOf(lat), shortDesc, longDesc, imgUrl));}
+
+                        mSighsAdapter = new SightsAdapter(Sights.this,mSightList);
+                        mRecyclerview.setAdapter(mSighsAdapter);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mRequestQueue.add(request);
     }
 }
