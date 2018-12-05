3d4357ce4bca007661001a81253dc630ada64ca2
CedricVanBroek <cedricvanbroek@gmail.com>
2018-12-05 11:24:15 +0100

    changed dotsmodel and dotscontroller to streetmodel and streetscontroller

--- src/ASPCoreApi/ASPCoreApi/Migrations/20181205101658_init.Designer.cs
+++ src/ASPCoreApi/ASPCoreApi/Migrations/20181205101658_init.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASPCoreApi.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    [Migration("20181127200242_skinId")]
-    partial class skinId
+    [Migration("20181205101658_init")]
+    partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -20,23 +20,6 @@ namespace ASPCoreApi.Migrations
                 .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-            modelBuilder.Entity("ASPCoreApi.Models.Dot", b =>
-                {
-                    b.Property<int>("id")
-                        .ValueGeneratedOnAdd()
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<string>("Latitude");
-
-                    b.Property<string>("Longitude");
-
-                    b.Property<bool>("Taken");
-
-                    b.HasKey("id");
-
-                    b.ToTable("dots");
-                });
 
             modelBuilder.Entity("ASPCoreApi.Models.GameStats", b =>
                 {
@@ -101,6 +84,27 @@ namespace ASPCoreApi.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("stats");
+                });
+
+            modelBuilder.Entity("ASPCoreApi.Models.Streets", b =>
+                {
+                    b.Property<int>("id")
+                        .ValueGeneratedOnAdd()
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("LatitudeA");
+
+                    b.Property<string>("LatitudeB");
+
+                    b.Property<string>("LongitudeA");
+
+                    b.Property<string>("LongitudeB");
+
+                    b.Property<bool>("Taken");
+
+                    b.HasKey("id");
+
+                    b.ToTable("streets");
                 });
 
             modelBuilder.Entity("ASPCoreApi.Models.Users", b =>
